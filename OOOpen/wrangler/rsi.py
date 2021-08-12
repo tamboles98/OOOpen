b30c3803c54bc202f7dff3f02fbc68911bbcd1a0
tamboles98 <tamboles98@gmail.com>
2021-08-12 17:56:16 +0200

    fixed: a typo rsi not rfi

--- OOOpen/wrangler/rsi.py
+++ OOOpen/wrangler/rsi.py
@@ -1,7 +1,7 @@
 from pandas import DataFrame
 
-def rfi(data: DataFrame, nd: int = 15, open_col: str = "open", close_col: str = "close") -> DataFrame:
-    """Add a new column to data with the rfi oscilator
+def rsi(data: DataFrame, nd: int = 15, open_col: str = "open", close_col: str = "close") -> DataFrame:
+    """Add a new column to data with the rsi oscilator
 
     Parameters
     ----------
@@ -9,7 +9,7 @@ def rfi(data: DataFrame, nd: int = 15, open_col: str = "open", close_col: str = 
         A pandas DataFrame with stock information, in need to have columns with open
         a close values for each day and be ordered from older to newer values
     nd : int, optional
-        Time frame (number of days) to use when computing the rfi, by default 15
+        Time frame (number of days) to use when computing the rsi, by default 15
     open_col : str, optional
         Name of the column to use as open values, by default "open"
     close_col : str, optional
@@ -18,9 +18,9 @@ def rfi(data: DataFrame, nd: int = 15, open_col: str = "open", close_col: str = 
     Returns
     -------
     DataFrame
-        The original DataFrame (data) with the new rfi column
+        The original DataFrame (data) with the new rsi column
     """
     dif = data[open_col] - data[close_col]
     w = dif.clip(lower=0).rolling(nd).sum()
     l = -dif.clip(upper=0).rolling(nd).sum()
-    return data.assign(**{"rfi{}".format(nd): 100 - 100/(1 + w/l)})
+    return data.assign(**{"rsi{}".format(nd): 100 - 100/(1 + w/l)})
